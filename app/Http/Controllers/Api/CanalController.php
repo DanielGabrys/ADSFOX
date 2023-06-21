0288dbeaa1954326757ee0424f81c9fe182a0fb4
DanielGabrys <daniello.gab@gmail.com>
2023-06-21 09:32:55 +0200

    create rest methods

---
+++ app/Http/Controllers/Api/CanalController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCanalRequest;
+use App\Http\Requests\UpdateCanalRequest;
+use App\Http\Resources\CanalResource;
+use App\Models\Canal;
+use Illuminate\Http\Request;
+
+class CanalController extends Controller
+{
+    public function index()
+    {
+        return CanalResource::collection(Canal::paginate(10));
+    }
+
+    public function show(Canal $canal)
+    {
+        return new CanalResource($canal);
+    }
+
+
+    public function store(StoreCanalRequest $request)
+    {
+        Canal::create($request->validated());
+    }
+
+    public function update(StoreCanalRequest $request, Canal $canal)
+    {
+
+        $canal->update($request->validated());
+        return response()->json("updated");
+    }
+
+    public function destroy(Canal $canal)
+    {
+        $canal->delete();
+    }
+}
